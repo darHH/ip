2becd81ede2101178ed71e1fb51dacf3fa7cdab8
Darren Teo HH <darrenteo77@gmail.com>
2025-01-23 00:35:48 +0800

    Created Task class

---
+++ src/main/Task.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    //...
+}
+    
