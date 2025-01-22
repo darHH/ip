82dac28bdbfd2d624a61b627dd04792477ab3d12
Darren Teo HH <darrenteo77@gmail.com>
2025-01-22 23:50:45 +0800

    Added greeting and exit message

---
+++ src/main/java/Dar.java
@@ -0,0 +1,7 @@
+public class Dar {
+    public static void main(String[] args) {
+        String greetingMessage = "Hey buddy! The name's Dar, what can I do for you today?";
+        String ExitMessage = "I'll see ya around, take it easy bud!";
+        System.out.println(greetingMessage + "\n" + ExitMessage);
+    }
+}
