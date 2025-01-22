959b643a0dced7aba80fc84f7d52a2645e79adb3
Darren Teo HH <darrenteo77@gmail.com>
2025-01-23 00:06:31 +0800

    Echo function implemented

--- src/main/java/Dar.java
+++ src/main/java/Dar.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class Dar {
     public static void main(String[] args) {
+        // Greeting
         String greetingMessage = "Hey buddy! The name's Dar, what can I do for you today?";
-        String ExitMessage = "I'll see ya around, take it easy bud!";
-        System.out.println(greetingMessage + "\n" + ExitMessage);
+        String exitMessage = "I'll see ya around, take it easy bud!";
+        System.out.println(greetingMessage + "\n");
+
+        // Main 
+        String inputState = "";
+        Scanner scanner = new Scanner(System.in); // Create a Scanner object
+        while (true) { 
+            inputState = scanner.nextLine(); // Read a string input
+            if (!inputState.equalsIgnoreCase("BYE")) { 
+                System.out.println("\n" + inputState + "\n");
+            } else {
+                break; 
+            }
+        }
+        // Exit 
+        scanner.close();
+        System.out.println("\n" + exitMessage);
+
     }
 }
