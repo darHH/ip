65ace5f6118d69d93813f7755585c3948338d93e
Darren Teo HH <darrenteo77@gmail.com>
2025-01-23 17:06:15 +0800

    Added instruction set and integrated Task class to Dar

--- src/main/java/Dar.java
+++ src/main/java/Dar.java
@@ -1,33 +1,56 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Dar {
+
+    private static final HashMap<String, Runnable> instructionMap = new HashMap<>(); // Instruction set
+    private static final ArrayList<Task> userList = new ArrayList<>(); // List of tasks
+
+
     public static void main(String[] args) {
         // Greeting
         String greetingMessage = "Hey buddy! The name's Dar, what can I do for you today?";
-        String exitMessage = "I'll see ya around, take it easy bud!";
         System.out.println(greetingMessage + "\n");
 
         // Main 
-        ArrayList<String> userList = new ArrayList<>(); // Create a dynamic array
+        instructionMap.put("mark", Dar::mark);
+        instructionMap.put("unmark", Dar::unmark);
+        instructionMap.put("list", Dar::list);
+
         Scanner scanner = new Scanner(System.in); // Create a Scanner object
+
         while (true) { 
             String inputState = scanner.nextLine(); // Read a string input
-            if (inputState.equalsIgnoreCase("LIST")) {
-                System.out.println("Here's the current list:");
-                for (int i = 0; i < userList.size(); i++) {
-                    System.out.println((i + 1) + ". " + userList.get(i)); 
-                } 
-            } else if (!inputState.equalsIgnoreCase("BYE")) { 
-            userList.add(inputState);
-            System.out.println("Gotcha! Added: " + inputState + "\n"); 
+            String[] inputParts = inputState.split(" "); // Split input into parts
+            String firstWord = inputParts[0].toLowerCase(); // Extract the first word
+
+            if (instructionMap.containsKey(firstWord)) {
+                    instructionMap.get(firstWord).run(); // Execute the instruction
+            } else if (!firstWord.equals("bye")) {
+            Task userTask = new Task(inputState);
+            userList.add(userTask); // Add the Task instance to the list
             } else {
                 break; 
             }
         }
         // Exit 
         scanner.close();
+        String exitMessage = "I'll see ya around, take it easy bud!";
         System.out.println("\n" + exitMessage);
+
+    }
+    private static void list() {
+        System.out.println("listing...");
+
+    }
+
+    private static void mark() {
+        System.out.println("marking...");
+    }
+
+    private static void unmark() {
+        System.out.println("unmarking...");
 
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -3,16 +3,28 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    static int totalTasks = 0;
+    private int taskNumber;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        totalTasks++;
+        this.taskNumber = totalTasks;
+        System.out.println("Gotcha! Added: " + description + "\n"); 
     }
 
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
-    //...
+    public int getTaskNumber() {
+        return this.taskNumber;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
 }
     
